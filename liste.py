5a9579794afffd2656c7f4c3ad343e3b159ae027
ysulun41-coder <ysulun41@gmail.com>
2026-05-11 08:53:59 +0300

    Create liste.py

---
+++ liste.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+import sqlite3
+
+def goster(db_yolu):
+    st.subheader("Daire Detay Listesi")
+    conn = sqlite3.connect(db_yolu)
+    df_full = pd.read_sql_query("SELECT blok as Blok, daire_no as Daire, malik_ad as Malik, malik_tc as 'Malik TC', malik_tel as Telefon, kiraci_ad as Kiracı, kiraci_tc as 'Kiracı TC', kiraci_tel as 'Kiracı Tel', plaka as Plaka, sifre as Şifre FROM sakinler", conn)
+    conn.close()
+    if not df_full.empty: 
+        st.dataframe(df_full, use_container_width=True, hide_index=True)
+    else: 
+        st.info("Kayıt yok.")
